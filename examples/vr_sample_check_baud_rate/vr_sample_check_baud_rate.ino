c0e60f996acb075a105ec7390a8381f79a102cd4
JiapengLi <gapleehit@gmail.com>
2013-07-10 15:02:17 +0800

    Add Check baud rate sample.

---
+++ examples/vr_sample_check_baud_rate/vr_sample_check_baud_rate.ino
@@ -0,0 +1,52 @@
+/**
+ ******************************************************************************
+ * @file    vr_sample_control_led.ino
+ * @author  JiapengLi
+ * @brief   This file provides a demostration on 
+ *     checking the baud rate of VoiceRecognitionModule
+ ******************************************************************************
+ * @note:
+ * check baud rate
+ ******************************************************************************
+ * @section  HISTORY
+ * 
+ * 2013/07/10    Initial version.
+ */
+#include <SoftwareSerial.h>
+#include "VoiceRecognitionV2.h"  
+/**        
+ * Connection
+ * Arduino    VoiceRecognitionModule
+ * 2   ------->     TX
+ * 3   ------->     RX
+ */
+VR myVR(2,3);    // 2:RX 3:TX, you can choose your favourite pins.
+
+int br[]={
+  2400, 4800, 9600, 19200, 38400
+};
+
+void setup(void)
+{
+  /** initialize */
+  int i=0;
+  Serial.begin(115200);
+  Serial.println("Elechouse Voice Recognition V2 Module\r\nCheck Baud Rate sample");
+  for(i=0; i<5; i++){
+    myVR.begin(br[i]);
+    if(myVR.clear() == 0){
+      Serial.print("Baud rate: ");
+      Serial.println(br[i], DEC);
+      break;
+    }
+  }
+  if(i==5){
+    Serial.println("Check baud rate failed. \r\nPlease check the connection, and reset arduino");
+  }
+}
+
+void loop(void)
+{
+
+}
+
